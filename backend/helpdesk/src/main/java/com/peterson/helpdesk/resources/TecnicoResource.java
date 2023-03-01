90df3695c7d465b3e8a19c82b24757304907793f
PetScaramussi <peterson.silva6@fatec.sp.gov.br>
2023-03-01 13:52:00 -0300

    DTO pattern Tecnico

--- backend/helpdesk/src/main/java/com/peterson/helpdesk/resources/TecnicoResource.java
+++ backend/helpdesk/src/main/java/com/peterson/helpdesk/resources/TecnicoResource.java
@@ -1,6 +1,7 @@
 package com.peterson.helpdesk.resources;
 
 import com.peterson.helpdesk.domain.Tecnico;
+import com.peterson.helpdesk.domain.dtos.TecnicoDTO;
 import com.peterson.helpdesk.services.TecnicoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -19,9 +20,9 @@ public class TecnicoResource {
     private TecnicoService service;
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<Tecnico> findById(@PathVariable Integer id) {
+    public ResponseEntity<TecnicoDTO> findById(@PathVariable Integer id) {
         Tecnico obj = this.service.findById(id);
-        return ResponseEntity.ok().body(obj);
+        return ResponseEntity.ok().body(new TecnicoDTO(obj));
     }
 
 }
