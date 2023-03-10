32253fb84d0d31a60aa56675c84b3ecf46a02a83
PetScaramussi <peterson.silva6@fatec.sp.gov.br>
2023-03-09 21:20:11 -0300

    findById for Chamado

---
+++ backend/helpdesk/src/main/java/com/peterson/helpdesk/resources/ChamadoResource.java
@@ -0,0 +1,25 @@
+package com.peterson.helpdesk.resources;
+
+import com.peterson.helpdesk.domain.Chamado;
+import com.peterson.helpdesk.domain.dtos.ChamadoDTO;
+import com.peterson.helpdesk.services.ChamadoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/chamado")
+public class ChamadoResource {
+
+    @Autowired
+    private ChamadoService service;
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<ChamadoDTO> findById(@PathVariable Integer id) {
+        Chamado obj = service.findById(id);
+        return ResponseEntity.ok().body(new ChamadoDTO(obj));
+    }
+}
---
+++ backend/helpdesk/src/main/java/com/peterson/helpdesk/services/ChamadoService.java
@@ -0,0 +1,24 @@
+package com.peterson.helpdesk.services;
+
+
+import com.peterson.helpdesk.domain.Chamado;
+import com.peterson.helpdesk.repositories.ChamadoRepository;
+import com.peterson.helpdesk.services.exceptions.ObjectnotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class ChamadoService {
+
+    @Autowired
+    private ChamadoRepository repository;
+
+    public Chamado findById(Integer id) {
+        Optional<Chamado> obj = repository.findById(id);
+        return obj.orElseThrow(() -> new ObjectnotFoundException("Objeto não encontrado! ID: " + id));
+    }
+
+
+}
