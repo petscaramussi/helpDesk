f388ef13cf3224c1d16a2bf119363cfa3593f0e2
PetScaramussi <peterson.silva6@fatec.sp.gov.br>
2023-03-09 21:36:15 -0300

    findAll for Chamado

--- backend/helpdesk/src/main/java/com/peterson/helpdesk/resources/ChamadoResource.java
+++ backend/helpdesk/src/main/java/com/peterson/helpdesk/resources/ChamadoResource.java
@@ -5,10 +5,11 @@ import com.peterson.helpdesk.domain.dtos.ChamadoDTO;
 import com.peterson.helpdesk.services.ChamadoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/chamado")
@@ -22,4 +23,13 @@ public class ChamadoResource {
         Chamado obj = service.findById(id);
         return ResponseEntity.ok().body(new ChamadoDTO(obj));
     }
+
+    @GetMapping
+    public ResponseEntity<List<ChamadoDTO>> findAll() {
+        List<Chamado> list = service.findAll();
+        List<ChamadoDTO> listDTO = list.stream().map(obj -> new ChamadoDTO(obj)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDTO);
+    }
+
+   //
 }
--- backend/helpdesk/src/main/java/com/peterson/helpdesk/services/ChamadoService.java
+++ backend/helpdesk/src/main/java/com/peterson/helpdesk/services/ChamadoService.java
@@ -7,6 +7,7 @@ import com.peterson.helpdesk.services.exceptions.ObjectnotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -21,4 +22,7 @@ public class ChamadoService {
     }
 
 
+    public List<Chamado> findAll() {
+        return repository.findAll();
+    }
 }
