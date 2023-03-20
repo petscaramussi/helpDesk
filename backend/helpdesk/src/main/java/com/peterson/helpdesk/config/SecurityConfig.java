09fe28dc36c161f7ce4fcebdabd0cf2f610db727
PetScaramussi <peterson.silva6@fatec.sp.gov.br>
2023-03-20 12:24:54 -0300

    implements JWT Authorization

--- backend/helpdesk/src/main/java/com/peterson/helpdesk/config/SecurityConfig.java
+++ backend/helpdesk/src/main/java/com/peterson/helpdesk/config/SecurityConfig.java
@@ -1,11 +1,13 @@
 package com.peterson.helpdesk.config;
 
 import com.peterson.helpdesk.security.JWTAuthenticationFilter;
+import com.peterson.helpdesk.security.JWTAuthoorizationFilter;
 import com.peterson.helpdesk.security.JWTUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -19,6 +21,7 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import java.util.Arrays;
 
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private static final String[] PUBLIC_MATCHERS = {"/h2-console/**"};
@@ -39,6 +42,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
         http.cors().and().csrf().disable();
         http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+        http.addFilter(new JWTAuthoorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
         http.authorizeRequests().antMatchers(PUBLIC_MATCHERS).permitAll().anyRequest().authenticated();
 
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
--- backend/helpdesk/src/main/java/com/peterson/helpdesk/resources/TecnicoResource.java
+++ backend/helpdesk/src/main/java/com/peterson/helpdesk/resources/TecnicoResource.java
@@ -5,6 +5,7 @@ import com.peterson.helpdesk.domain.dtos.TecnicoDTO;
 import com.peterson.helpdesk.services.TecnicoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -34,7 +35,7 @@ public class TecnicoResource {
         List<TecnicoDTO> listDTO = list.stream().map(obj -> new TecnicoDTO(obj)).collect(Collectors.toList());
          return ResponseEntity.ok().body(listDTO);
     }
-
+    @PreAuthorize("hasAnyRole('ADMIN')")
     @PostMapping
     public ResponseEntity<TecnicoDTO> create(@Valid @RequestBody TecnicoDTO objDTO) {
         Tecnico newObj = service.create(objDTO);
@@ -42,12 +43,14 @@ public class TecnicoResource {
         return ResponseEntity.created(uri).build();
     }
 
+    @PreAuthorize("hasAnyRole('ADMIN')")
     @PutMapping(value = "/{id}")
     public ResponseEntity<TecnicoDTO> update(@PathVariable Integer id, @Valid @RequestBody TecnicoDTO objDTO) {
         Tecnico obj = service.update(id, objDTO);
         return ResponseEntity.ok().body(new TecnicoDTO(obj));
     }
 
+    @PreAuthorize("hasAnyRole('ADMIN')")
     @DeleteMapping(value = "{id}")
     public ResponseEntity<TecnicoDTO> delete(@PathVariable Integer id) {
         service.delete(id);
