36116bcb16650a64805542dd4ce05f0d691fc44e
PetScaramussi <peterson.silva6@fatec.sp.gov.br>
2023-04-05 20:35:34 -0300

    implements Logout

--- frontend/helpdesk/src/app/components/nav/nav.component.ts
+++ frontend/helpdesk/src/app/components/nav/nav.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-nav',
@@ -9,12 +12,21 @@ export class NavComponent implements OnInit {
 
  
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private authService: AuthService,
+    private toast: ToastrService) { }
 
   ngOnInit(): void {
     
   }
 
   showFiller = false;
+
+  logout() {
+    this.router.navigate(['/login']);
+    this.authService.logout();
+    this.toast.info('Logout realizado com sucesso', 'Logout', {timeOut: 7000});
+  }
 
 }
