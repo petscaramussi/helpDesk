afe67fb276f3fb8e68e1e92b2a06fc39a8b73a0d
PetScaramussi <peterson.silva6@fatec.sp.gov.br>
2023-04-10 17:20:12 -0300

    Finishing Tecnico-list

--- frontend/helpdesk/src/app/interceptors/auth.interceptor.ts
+++ frontend/helpdesk/src/app/interceptors/auth.interceptor.ts
@@ -17,7 +17,7 @@ export class AuthInterceptor implements HttpInterceptor {
     let token = localStorage.getItem('token');
 
     if(token) {
-      const cloneReq = request.clone({headers: request.headers.set('Authorization', token)});
+      const cloneReq = request.clone({headers: request.headers.set('Authorization', `Bearer ${token}`)});
       return next.handle(cloneReq);
     } else {
       return next.handle(request);
