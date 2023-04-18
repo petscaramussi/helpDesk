cfd092b0fe0276f8e3de044601162659f256f3d9
PetScaramussi <peterson.silva6@fatec.sp.gov.br>
2023-04-18 12:38:56 -0300

    finish CREATE TECNICO

--- frontend/helpdesk/src/app/components/tecnico/tecnico-create/tecnico-create.component.ts
+++ frontend/helpdesk/src/app/components/tecnico/tecnico-create/tecnico-create.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Tecnico } from 'src/app/models/tecnico';
+import { TecnicoService } from 'src/app/services/tecnico.service';
 
 @Component({
   selector: 'app-tecnico-create',
@@ -8,19 +12,60 @@ import { FormControl, Validators } from '@angular/forms';
 })
 export class TecnicoCreateComponent implements OnInit {
 
+  tecnico: Tecnico = {
+    id: '',
+    nome: '',
+    cpf: '',
+    email: '',
+    senha: '',
+    perfis: [],
+    dataCriacao: ''
+  }
+
   nome: FormControl = new FormControl(null, Validators.minLength(3));
   cpf: FormControl = new FormControl(null, Validators.required);
   email: FormControl = new FormControl(null, Validators.email);
   senha: FormControl = new FormControl(null, Validators.minLength(3));
 
 
-  constructor() { }
+  constructor(
+    private service: TecnicoService,
+    private toast: ToastrService,
+    private router: Router) { }
 
   ngOnInit(): void {
   }
 
   validaCampos(): boolean {
     return this.nome.valid && this.cpf.valid && this.email.valid && this.senha.valid
+  }
+
+  create(): void {
+    this.service.create(this.tecnico).subscribe({
+      next: () => {
+      this.toast.success('Técnico cadastrado com sucesso', 'Cadastro');
+      this.router.navigate(['tecnicos']);
+      },
+      error: (erro) => {
+        if(erro.error.errors) {
+          erro.error.errors.forEach(element => {
+            this.toast.error(element.message);
+          });
+        } else {
+          this.toast.error(erro.error.message);
+        }      
+      }
+  })
+  }
+
+  addPerfil(perfil: any): void {
+    if(this.tecnico.perfis.includes(perfil)) {
+      this.tecnico.perfis.splice(this.tecnico.perfis.indexOf(perfil), 1);
+      console.log(this.tecnico.perfis);
+    } else {
+      this.tecnico.perfis.push(perfil);
+      console.log(this.tecnico.perfis);
+    }
   }
 
 }
