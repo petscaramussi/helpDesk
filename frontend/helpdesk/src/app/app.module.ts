1f2974a361af92f3bf4f9a0850fba2fcd9d50385
PetScaramussi <peterson.silva6@fatec.sp.gov.br>
2023-04-24 13:06:48 -0300

    Implements Cliente CRUD

--- frontend/helpdesk/src/app/app.module.ts
+++ frontend/helpdesk/src/app/app.module.ts
@@ -39,6 +39,10 @@ import { TecnicoCreateComponent } from './components/tecnico/tecnico-create/tecn
 import { NgxMaskModule } from 'ngx-mask';
 import { TecnicoUpdateComponent } from './components/tecnico/tecnico-update/tecnico-update.component';
 import { TecnicoDeleteComponent } from './components/tecnico/tecnico-delete/tecnico-delete.component';
+import { ClienteCreateComponent } from './components/cliente/cliente-create/cliente-create.component';
+import { ClienteUpdateComponent } from './components/cliente/cliente-update/cliente-update.component';
+import { ClienteListComponent } from './components/cliente/cliente-list/cliente-list.component';
+import { ClienteDeleteComponent } from './components/cliente/tecnico-delete/cliente-delete.component';
 
 @NgModule({
   declarations: [
@@ -50,7 +54,11 @@ import { TecnicoDeleteComponent } from './components/tecnico/tecnico-delete/tecn
     LoginComponent,
     TecnicoCreateComponent,
     TecnicoUpdateComponent,
-    TecnicoDeleteComponent
+    TecnicoDeleteComponent,
+    ClienteCreateComponent,
+    ClienteListComponent,
+    ClienteUpdateComponent,
+    ClienteDeleteComponent
   ],
   imports: [
     BrowserModule,
---
+++ frontend/helpdesk/src/app/components/cliente/tecnico-delete/cliente-delete.component.ts
@@ -0,0 +1,64 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Cliente } from 'src/app/models/cliente';
+import { ClienteService } from 'src/app/services/cliente.service';
+
+@Component({
+  selector: 'app-cliente-delete',
+  templateUrl: './cliente-delete.component.html',
+  styleUrls: ['./cliente-delete.component.css']
+})
+export class ClienteDeleteComponent implements OnInit {
+
+  cliente: Cliente = {
+    id: '',
+    nome: '',
+    cpf: '',
+    email: '',
+    senha: '',
+    perfis: [],
+    dataCriacao: ''
+  }
+
+
+  constructor(
+    private service: ClienteService,
+    private toast: ToastrService,
+    private router: Router,
+    private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.cliente.id = this.route.snapshot.paramMap.get('id');
+    this.findById();
+  }
+
+  findById(): void {
+    this.service.findById(this.cliente.id).subscribe(resposta => {
+      resposta.perfis = [];
+      this.cliente = resposta;
+    })
+  }
+
+  delete(): void {
+    this.service.delete(this.cliente.id).subscribe({
+      next: () => {
+        this.toast.success('Cliente deletado com sucesso', 'Delete');
+        this.router.navigate(['clientes']);
+      },
+      error: (erro) => {
+        if (erro.error.errors) {
+          erro.error.errors.forEach(element => {
+            this.toast.error(element.message);
+          });
+        } else {
+          this.toast.error(erro.error.message);
+        }
+      }
+    })
+  }
+
+  
+
+}
